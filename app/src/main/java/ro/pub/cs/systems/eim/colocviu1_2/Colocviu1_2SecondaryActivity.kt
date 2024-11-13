5fe193cdb0090c53408d599f1573c000343fb491
Corneliu <corneliu.rotari>
2024-11-13 17:24:26 +0200

    done C1

---
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Colocviu1_2SecondaryActivity.kt
@@ -0,0 +1,24 @@
+package ro.pub.cs.systems.eim.colocviu1_2
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Colocviu1_2SecondaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_colocviu12_secondary)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val reccv : Int = intent.getStringExtra("sum").toString().split(" + ").sumOf { it.toInt() };
+        Toast.makeText(this, reccv.toString(), Toast.LENGTH_SHORT).show()
+    }
+}
--- app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/MainActivity.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/MainActivity.kt
@@ -1,13 +1,24 @@
 package ro.pub.cs.systems.eim.colocviu1_2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
+import ro.pub.cs.systems.eim.colocviu1_2.R.id.add
+import ro.pub.cs.systems.eim.colocviu1_2.R.id.compute
+import ro.pub.cs.systems.eim.colocviu1_2.R.id.nextTerm
+import ro.pub.cs.systems.eim.colocviu1_2.R.id.allTerms
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -16,5 +27,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val compute : Button = findViewById(compute);
+        val add : Button= findViewById(add);
+        val nextTerm: EditText = findViewById(nextTerm);
+        val allTerms: TextView = findViewById(allTerms);
+
+        var added : String = "";
+
+        add.setOnClickListener {
+            val readed : String = nextTerm.text.toString();
+            if (readed.toIntOrNull() != null) {
+                if (added == "") {
+                    allTerms.text = added.plus(readed);
+                }
+                else {
+                    allTerms.text = added.plus(" + ".plus(readed));
+                }
+                added = allTerms.text.toString();
+            }
+        }
+
+        compute.setOnClickListener {
+            val intent = Intent(this, Colocviu1_2SecondaryActivity::class.java)
+            intent.putExtra("sum", added)
+            startActivity(intent)        }
+
     }
 }
